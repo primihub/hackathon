cf60e54786b68b622daa3a35c83ed36cf5434059
zhanjingjing <zhanjingjing@primihub.com>
2022-10-18 13:17:01 +0800

    add partners img

--- src/views/PcIndex.vue
+++ src/views/PcIndex.vue
@@ -238,7 +238,8 @@
           <img src="../assets/images/partner-logo-mulan.png" alt="木兰开源社区">
           <img src="../assets/images/partner-logo-kubesphere.svg" alt="木兰开源社区">
           <img src="../assets/images/partner-logo-kaiyuanshe.png" alt="开源社">
-          <img src="../assets/images/placeholder.png" alt="Datawhale 开源社区">
+          <div class="placeholder-img">欢迎加入</div>  
+          <!-- <img src="../assets/images/placeholder.png" alt="Datawhale 开源社区"> -->
         </div>
       </div>
       <div class="partners-item">
